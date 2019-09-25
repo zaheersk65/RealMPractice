36a74aa40f919f97379698315858182c4b1d68b2
zaheersk65 <zaheersk65@gmail.com>
2019-09-25 10:37:02 +0530

    Initial Commit

---
+++ RealMPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealMPractice
+//
+//  Created by IMAC on 9/25/19.
+//  Copyright © 2019 IMAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
